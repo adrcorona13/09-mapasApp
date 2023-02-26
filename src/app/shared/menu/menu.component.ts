7368f8fee040e27e0f910626b6fdcaccd7171490
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-26 01:47:41 -0600

    agregar menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem{
+  texto: string;
+  ruta: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
 export class MenuComponent {
 
+  menu: MenuItem[] = [
+    { texto: 'Fullscreen', ruta: 'mapas/fullscreen'},
+    { texto: 'Zoom Range', ruta: 'mapas/zoom-range'},
+    { texto: 'Marcadores', ruta: 'mapas/marcadores'},
+    { texto: 'Propiedades', ruta: 'mapas/propiedades'},
+  ]
 }
