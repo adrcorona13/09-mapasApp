aeab9a5cca782ac401854cb2c73c3082b8ce1c02
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-27 15:28:13 -0600

    agregar comportamiento del zoom

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -19,19 +19,52 @@ import * as mapboxgl from 'mapbox-gl';
         padding: 10px;
         border-radius: 5px;
         z-index: 9999;
+        width:400px
       }
     `
   ]
 })
-export class ZoomRangeComponent implements OnInit{
+export class ZoomRangeComponent implements AfterViewInit{
   
-  ngOnInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'mapa', // container ID
+  
+  @ViewChild('mapa')
+  mapContainer!: ElementRef;
+  
+  map!: mapboxgl.Map;
+
+  zoomLevel: number = 10;
+
+  ngAfterViewInit(): void {
+    console.log(this.mapContainer);
+    
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-99.1240, 19.4426], // starting position [lng, lat]
-      zoom: 17, // starting zoom
+      zoom: this.zoomLevel, // starting zoom
     });
+
+    this.map.on('zoom', (ev) => {
+      this.zoomLevel = this.map.getZoom();
+    })
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map.getZoom() > 18) {
+       this.map.zoomTo(18); 
+      }
+    })
   }
 
+  zoom(inOrOut: string){
+    
+    if (inOrOut === "in") {
+      this.map.zoomIn();
+    }else{
+      this.map.zoomOut();
+    }   
+  }
+
+  zoomCambio(valor: string){
+    this.map.zoomTo(Number(valor));
+  }
 }
